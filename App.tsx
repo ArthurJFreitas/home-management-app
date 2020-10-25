7460893fb6a240a9a7678051476869b05e4d8b36
Arthur F <arthurfscode@gmail.com>
2020-10-24 23:28:32 -0300

    :tada: first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      <View>
+          <Text>
+            aaaaaaaaaaaaaaaaaaaaa
+          </Text>
+      </View>
+    </>
+  );
+};
+
+
+export default App;
