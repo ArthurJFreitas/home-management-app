79e4464cf1815ed08b29a36164a7593ebd1d23e8
nicolas <nicolasprodmac@gmail.com>
2020-11-01 22:48:22 -0300

    :sparkles: feat: added bottom sheet modal

--- src/pages/to-do-page/index.tsx
+++ src/pages/to-do-page/index.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import { View, TouchableOpacity, Button, TextInput } from 'react-native';
+
 import { Formik } from 'formik';
 import * as yup from 'yup';
+import Animated from 'react-native-reanimated';
+import BottomSheet from 'reanimated-bottom-sheet';
+
 import Text from '../../components/Text';
+
 import * as Styled from './styles';
 
 interface ToDoProps {
@@ -11,6 +16,7 @@ interface ToDoProps {
 
 const ToDoPage = () => {
   const [toDoList, setToDoList] = useState<ToDoProps[]>([]);
+  const [showBottomModal, setShowBottomModal] = useState(false);
 
   const validationSchema = yup.object().shape({
     title: yup
@@ -19,38 +25,14 @@ const ToDoPage = () => {
       .min(3, 'O titulo precisa de 3 letras pelo menos'),
   });
 
-  const handleRemove = (title: string) => {
-    const newList = toDoList.filter((item: ToDoProps) => item.title !== title);
-    setToDoList(newList);
-  };
-
-  return (
-    <Styled.Container>
-      <Styled.DaysOfTheWeekWrapper>
-        <Styled.DayWrapper>
-          <Text size="18" weight="Bold">
-            Segunda feira
-          </Text>
-          <Text size="18" weight="Light">
-            01/11
-          </Text>
-        </Styled.DayWrapper>
-        {toDoList.map(({ title }) => {
-          return (
-            <Styled.ToDo key={title}>
-              <Text>{title}</Text>
-              <TouchableOpacity onPress={() => handleRemove(title)}>
-                <Styled.Remove>
-                  <Text size="18" weight="Bold" color="#fc0000">
-                    X
-                  </Text>
-                </Styled.Remove>
-              </TouchableOpacity>
-            </Styled.ToDo>
-          );
-        })}
-      </Styled.DaysOfTheWeekWrapper>
-
+  const renderContent = () => (
+    <View
+      style={{
+        backgroundColor: 'white',
+        padding: 16,
+        height: 450,
+      }}
+    >
       <Formik
         validationSchema={validationSchema}
         validateOnBlur
@@ -84,7 +66,9 @@ const ToDoPage = () => {
               <Text>{errors?.title}</Text>
               <Styled.AddButton
                 onPress={() => {
-                  handleSubmit();
+                  sheetRef.current.snapTo(0);
+                  setShowBottomModal(!showBottomModal);
+                  // handleSubmit();
                 }}
               >
                 <Text color="#fff" size="50">
@@ -95,7 +79,63 @@ const ToDoPage = () => {
           );
         }}
       </Formik>
-    </Styled.Container>
+    </View>
+  );
+
+  const handleRemove = (title: string) => {
+    const newList = toDoList.filter((item: ToDoProps) => item.title !== title);
+    setToDoList(newList);
+  };
+
+  const sheetRef = React.useRef(null);
+
+  return (
+    <>
+      <Styled.Container>
+        <Styled.DaysOfTheWeekWrapper>
+          <Styled.DayWrapper>
+            <Text size="18" weight="Bold">
+              Segunda feira
+            </Text>
+            <Text size="18" weight="Light">
+              01/11
+            </Text>
+          </Styled.DayWrapper>
+          {toDoList.map(({ title }) => {
+            return (
+              <Styled.ToDo key={title}>
+                <Text>{title}</Text>
+                <TouchableOpacity onPress={() => handleRemove(title)}>
+                  <Styled.Remove>
+                    <Text size="18" weight="Bold" color="#fc0000">
+                      X
+                    </Text>
+                  </Styled.Remove>
+                </TouchableOpacity>
+              </Styled.ToDo>
+            );
+          })}
+        </Styled.DaysOfTheWeekWrapper>
+        <Styled.AddButton
+          onPress={() => {
+            setShowBottomModal(!showBottomModal);
+          }}
+        >
+          <Text color="#fff" size="50">
+            +
+          </Text>
+        </Styled.AddButton>
+      </Styled.Container>
+
+      {showBottomModal && (
+        <BottomSheet
+          ref={sheetRef}
+          snapPoints={[200, 300, 0]}
+          borderRadius={10}
+          renderContent={renderContent}
+        />
+      )}
+    </>
   );
 };
 
