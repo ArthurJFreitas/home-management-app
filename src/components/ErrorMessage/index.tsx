dbf2d81ad174a098fb6a70cb460904e3666822e9
nicolas <nicolasprodmac@gmail.com>
2020-11-01 23:22:05 -0300

    :sparkles: feat: added error message component

---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as Styled from './styles';
+
+const ErrorMessage = ({ children }: any) => {
+  return <Styled.ErrorMessage>{children}</Styled.ErrorMessage>;
+};
+
+export default ErrorMessage;
---
+++ src/components/ErrorMessage/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const ErrorMessage = styled.Text`
+  color: #e00d0d;
+  margin: 10px;
+`;
