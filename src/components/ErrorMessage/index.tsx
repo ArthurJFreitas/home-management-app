020ca05696ce796bd84c0709ae42b41f6aa02a53
nicolas <nicolasprodmac@gmail.com>
2020-11-01 23:47:02 -0300

    :bug: fix: fixed error message component

--- src/components/ErrorMessage/index.tsx
+++ src/components/ErrorMessage/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Text from '../Text';
+
 import * as Styled from './styles';
 
 const ErrorMessage = ({ children }: any) => {
-  return <Styled.ErrorMessage>{children}</Styled.ErrorMessage>;
+  return (
+    <Styled.ErrorMessage>
+      <Text color="#e00d0d">{children}</Text>
+    </Styled.ErrorMessage>
+  );
 };
 
 export default ErrorMessage;
--- src/components/ErrorMessage/styles.ts
+++ src/components/ErrorMessage/styles.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components/native';
 
 export const ErrorMessage = styled.Text`
-  color: #e00d0d;
   margin: 10px;
 `;
