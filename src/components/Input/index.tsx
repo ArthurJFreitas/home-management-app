d0f2527c85bc22f03b6489fb639c46093e87b1c9
nicolas <nicolasprodmac@gmail.com>
2020-11-01 23:35:56 -0300

    :sparkles: feat: added input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './styles';
+
+const Input = (props: any) => {
+  return <Styled.Input {...props} />;
+};
+
+export default Input;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Input = styled.TextInput`
+  height: 50px;
+  font-family: 'OpenSans-SemiBold';
+  background: #ebecf0;
+
+  margin-bottom: 13px;
+  margin-top: 15px;
+
+  padding: 0 15px;
+
+  width: 100%;
+  border-radius: 12px;
+`;
